57454e4dd507a01f5121b9b30788143991966fd6
Sydney Young <sydney.young@yale.edu>
2014-08-02 15:12:26 -0400

    added generate page

--- album_paper.rb
+++ album_paper.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
+require './AlbumArtScraper'
 
 get '/' do
   erb :index
 end
 
+post '/gen' do
+  images = AlbumArtScraper::Discogs::scrape_images(params[:artist], false)
+  @gen = AlbumArtScraper::WallpaperGen.new(params[:width].to_i, params[:height].to_i, images) if images
+  @gen.generate
+  erb :gen
+end
