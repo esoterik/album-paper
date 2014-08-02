5fa4cfa0df72ea1a5322ddf7273b9ad85f950bfb
Sydney Young <sydney.young@yale.edu>
2014-08-02 16:07:05 -0400

    singles checkbox now works

--- album_paper.rb
+++ album_paper.rb
@@ -9,10 +9,12 @@ post '/gen' do
   erb :gen
 end
 
-get '/gen/:artist/:width/:height' do
-  images = AlbumArtScraper::Discogs::scrape_images(params[:artist], false)
-  gen = AlbumArtScraper::WallpaperGen.new(params[:width].to_i, params[:height].to_i, images) if images
-  gen.generate
-  gen.wallpaper.to_blob
+get '/gen/:artist/:width/:height/:singles' do
+  images = AlbumArtScraper::Discogs::scrape_images(params[:artist], params[:singles])
+  if images
+    gen = AlbumArtScraper::WallpaperGen.new(params[:width].to_i, params[:height].to_i, images)
+    gen.generate
+    gen.wallpaper.to_blob
+  end
 end
 
