c2a7a0ccd6dc5b2101cc4885456f1cc7d6166e1a
Sydney Young <sydney.young@yale.edu>
2014-08-02 15:30:15 -0400

    separated image generating and image displaying into different routes

--- album_paper.rb
+++ album_paper.rb
@@ -6,8 +6,13 @@ get '/' do
 end
 
 post '/gen' do
-  images = AlbumArtScraper::Discogs::scrape_images(params[:artist], false)
-  @gen = AlbumArtScraper::WallpaperGen.new(params[:width].to_i, params[:height].to_i, images) if images
-  @gen.generate
   erb :gen
 end
+
+get '/gen/:artist/:width/:height' do
+  images = AlbumArtScraper::Discogs::scrape_images(params[:artist], false)
+  gen = AlbumArtScraper::WallpaperGen.new(params[:width].to_i, params[:height].to_i, images) if images
+  gen.generate
+  gen.wallpaper.to_blob
+end
+
